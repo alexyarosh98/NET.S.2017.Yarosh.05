0784b2e7caaa786dc56e7f707b9e9f30eb34fd12
Alexander <alexyarosh98@gmail.com>
2017-07-09 22:48:34 +0300

    Some operations for polynomials NUnit tests bugs are fixed

--- AlgorithmsAndPolynomials.NUnitTests/PolynomialTests.cs
+++ AlgorithmsAndPolynomials.NUnitTests/PolynomialTests.cs
@@ -10,11 +10,13 @@ namespace AlgorithmsAndPolynomials.NUnitTests
     [TestFixture]
     class PolynomialTests
     {
-        [TestCase(new double[]{1,2,-4,0},new double[]{1,-2,3,4,5},ExpectedResult = "+ 5*x^4 + 4*x^3 - 1*x^2 - 0*x^1 = 2")]
-        [TestCase(new double[] { 1, -2, 3, 4, 5 }, new double[] { 1, 2, -4, 0 }, ExpectedResult = "+ 5*x^4 + 4*x^3 - 1*x^2 - 0*x^1 = 2")]
-        [TestCase(new double[] { }, new double[] { 1, 2, 3 }, ExpectedResult = "+ 3*x^2 + 2*x^1 = 1")]
-        [TestCase(new double[]{1,2,3},new double[]{},ExpectedResult = "+ 3*x^2 + 2*x^1 = 1")]
-        [TestCase(new double[]{ 2.25, 4.15, 3.14, 6.20 },new double[]{ 1.123, 9, 4.51, 34, 12, double.MaxValue, double.MinValue},ExpectedResult = "- 1,79769313486232E+308*x^6 + 1,79769313486232E+308*x^5 + 12*x^4 + 40,2*x^3 + 7,65*x^2 + 13,15*x^1 = 3,373")]
+        [TestCase(new double[]{1,2,-4,0},new double[]{1,-2,3,4,5},ExpectedResult = "+ 5*x^4 + 4*x^3 - 1*x^2 - 0*x^1 = -2")]
+        [TestCase(new double[] { 1, -2, 3, 4, 5 }, new double[] { 1, 2, -4, 0 }, ExpectedResult = "+ 5*x^4 + 4*x^3 - 1*x^2 - 0*x^1 = -2")]
+        [TestCase(new double[] { }, new double[] { 1, 2, 3 }, ExpectedResult = "+ 3*x^2 + 2*x^1 = -1")]
+        [TestCase(new double[]{1,2,3},new double[]{},ExpectedResult = "+ 3*x^2 + 2*x^1 = -1")]
+        [TestCase(new double[]{1,2,3},new double[]{2,-2,-3},ExpectedResult = "0*x=-3")]
+        [TestCase(new double[]{ 2.25, 4.15, 3.14, 6.20 },new double[]{ 1.123, 9, 4.51, 34, 12, double.MaxValue, double.MinValue},ExpectedResult =
+            "- 1,79769313486232E+308*x^6 + 1,79769313486232E+308*x^5 + 12*x^4 + 40,2*x^3 + 7,65*x^2 + 13,15*x^1 = -3,373")]
         public string OperatorPlus_2Polinomials_AnotherPolyWichIsSumOfArguments(double[] obj1,double[] obj2)
         {
             Polynomial poly1=new Polynomial(obj1);
@@ -22,8 +24,8 @@ namespace AlgorithmsAndPolynomials.NUnitTests
 
             return (poly1 + poly2).ToString();
         }
-        [TestCase(new double[]{1,23,4},100,ExpectedResult = "+ 4*x^2 + 23*x^1 = 101")]
-        [TestCase(new double[]{},5,ExpectedResult = "0*x=5")]
+        [TestCase(new double[]{1,23,4},100,ExpectedResult = "+ 4*x^2 + 23*x^1 = -101")]
+        [TestCase(new double[]{},5,ExpectedResult = "0*x=-5")]
         
         public string OperatorPlus_PolynomialPlusDouble_NewPolynomial(double[] obj1, double value)
         {
@@ -31,8 +33,8 @@ namespace AlgorithmsAndPolynomials.NUnitTests
 
             return (poly+value).ToString();
         }
-        [TestCase(100, new double[] { 1, 23, 4 }, ExpectedResult = "+ 4*x^2 + 23*x^1 = 101")]
-        [TestCase( 5, new double[] { }, ExpectedResult = "0*x=5")]
+        [TestCase(100, new double[] { 1, 23, 4 }, ExpectedResult = "+ 4*x^2 + 23*x^1 = -101")]
+        [TestCase( 5, new double[] { }, ExpectedResult = "0*x=-5")]
         public string OperatorPlus_DoublePlusPolynomial_NewPolynomial(double value, double[] obj1)
         {
             Polynomial poly = new Polynomial(obj1);
@@ -42,14 +44,35 @@ namespace AlgorithmsAndPolynomials.NUnitTests
 
         [TestCase(new double[]{1,3,3,4,5},new double[]{1,2,7,4},ExpectedResult = "+ 5*x^4 - 0*x^3 - 4*x^2 + 1*x^1 = 0")]
         [TestCase(new double[] { 1, 2, 7, 4 }, new double[] { 1, 3, 3, 4, 5 }, ExpectedResult = "- 5*x^4 - 0*x^3 + 4*x^2 - 1*x^1 = 0")]
-        [TestCase(new double[]{},new double[]{1,2,3},ExpectedResult = "- 3*x^2 - 2*x^1 = -1")]
-        [TestCase(new double[]{1,2,3},new double[]{},ExpectedResult = "+ 3*x^2 + 2*x^1 = 1")]
+        [TestCase(new double[]{},new double[]{1,2,3},ExpectedResult = "- 3*x^2 - 2*x^1 = 1")]
+        [TestCase(new double[]{1,2,3},new double[]{},ExpectedResult = "+ 3*x^2 + 2*x^1 = -1")]
+        [TestCase(new double[]{1,2,3},new double[]{1,2,3},ExpectedResult = "0*x=0")]
         public string OperatorMinus_PolynomialMinusPolynomial_NewPolynomial(double[] obj1, double[] obj2)
         {
             Polynomial poly1=new Polynomial(obj1);
             Polynomial poly2=new Polynomial(obj2);
 
             return (poly1 - poly2).ToString();
+        }
+
+        [TestCase(new double[]{1,1,1,1},new double[]{2},ExpectedResult = "+ 2*x^3 + 2*x^2 + 2*x^1 = -2")]
+        [TestCase(new double[]{2},new double[]{1,1,1,1},ExpectedResult = "+ 2*x^3 + 2*x^2 + 2*x^1 = -2")]
+        [TestCase(new double[]{},new double[]{123,321,31,312},ExpectedResult = "0*x=0")]
+        public string OperatorMult_PolynomialMultByPolynomial_NewPolynimoal(double[] obj1, double[] obj2)
+        {
+            Polynomial poly1=new Polynomial(obj1);
+            Polynomial poly2=new Polynomial(obj2);
+
+            return (poly1 * poly2).ToString();
+        }
+
+        [TestCase(new double[] { 1, 1, 1, 1 }, 2, ExpectedResult = "+ 2*x^3 + 2*x^2 + 2*x^1 = -2")]
+        [TestCase(new double[]{12,31,312,3123},0,ExpectedResult = "0*x=0")]
+        public string OperatorMult_PolyBuDoubleValue_NewPoly(double[] obj, double value)
+        {
+            Polynomial poly=new Polynomial(obj);
+
+            return (poly * value).ToString();
         }
     }
 }
--- AlgorithmsAndPolynomials/Polynomial.cs
+++ AlgorithmsAndPolynomials/Polynomial.cs
@@ -1,15 +1,15 @@
 ﻿using System;
+using System.CodeDom;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AlgorithmsAndPolynomials
 {
     public class Polynomial
-        //
-        // Some methods, refactoring and operator "*" are coming soon 
-        //
     {
         public double[] Coefficients { get; }
         public Polynomial(params double[] coef)
@@ -22,22 +22,51 @@ namespace AlgorithmsAndPolynomials
         {
             Coefficients=new double[]{number};
         }
-        override public string ToString()
+        public override string ToString()
         {
             StringBuilder res = new StringBuilder();
-            if (Coefficients.Length == 1) return $"0*x={Coefficients[0]}";
+            if (Coefficients.Length == 1) return $"0*x={-1*Coefficients[0]}";
             for (int i = Coefficients.Length - 1; i >= 1; i--)
             {
-                res.Append(String.Format("{0} {1}*x^{2} {3}", Coefficients[i] > 0 ? "+" : "-", Math.Abs(Coefficients[i]), (i), i == 1 ? "= " + Coefficients[0] : ""));
+                res.Append(String.Format("{0} {1}*x^{2} {3}", Coefficients[i] > 0 ? "+" : "-", Math.Abs(Coefficients[i]),
+                    (i), i == 1 ? "= " + -1*Coefficients[0] : ""));
             }
-            return res.ToString();
 
+            return res.ToString();
         }
+
+        public override bool Equals(object poly)
+        {
+            if (poly == null) return false;
+            Polynomial polynomial = poly as Polynomial;
+            if (polynomial == null) return false;
+            if (polynomial.Coefficients.Length != Coefficients.Length) return false;
+
+            for (int i = 0; i < Coefficients.Length; i++)
+            {
+                if (Coefficients[i] != polynomial.Coefficients[i]) return false;
+            }
+
+            return true;
+        }
+
+        public Polynomial NormolizeThePoly()
+        {
+            if (Coefficients.Length == 1) return this;
+            double[] coeff=new double[Coefficients.Length];
+
+            for (int i = 0; i < Coefficients.Length; i++)
+            {
+                coeff[i] = Coefficients[i] / Coefficients[Coefficients.Length - 1];
+            }
+            return new Polynomial(coeff);
+        }
+
         public static Polynomial operator +(Polynomial obj1, Polynomial obj2)
         {
             if (obj1 == null || obj2 == null) throw new ArgumentException();
-            if(obj1.Coefficients.Length==0) return new Polynomial(obj2.Coefficients);
-            if(obj2.Coefficients.Length==0) return new Polynomial(obj1.Coefficients);
+            if(obj1.Coefficients.Length==0) return new Polynomial(obj2.Coefficients.Trim());
+            if(obj2.Coefficients.Length==0) return new Polynomial(obj1.Coefficients.Trim());
 
             int maxLength = Math.Max(obj1.Coefficients.Length, obj2.Coefficients.Length);
             double[] coeff = new double[maxLength];
@@ -60,7 +89,7 @@ namespace AlgorithmsAndPolynomials
                 }
             }
 
-            return new Polynomial(coeff);
+            return new Polynomial(coeff.Trim());
         }
         public static Polynomial operator +(Polynomial obj, double value)
         {
@@ -80,7 +109,7 @@ namespace AlgorithmsAndPolynomials
             {
                 double[] coeff=new double[obj2.Coefficients.Length];
                 for (int i = 0; i < obj2.Coefficients.Length; i++) coeff[i] = -obj2.Coefficients[i];
-                return new Polynomial(coeff);
+                return new Polynomial(coeff.Trim());
             }
             if(obj2.Coefficients.Length==0) return  new Polynomial(obj1.Coefficients);
 
@@ -92,7 +121,7 @@ namespace AlgorithmsAndPolynomials
                     coeff[i] = -obj2.Coefficients[i];
                 }
                 Polynomial newPoly = new Polynomial(coeff);
-                return new Polynomial((obj1 + newPoly).Coefficients);
+                return new Polynomial((obj1 + newPoly).Coefficients.Trim());
             }
             else
             {
@@ -102,11 +131,60 @@ namespace AlgorithmsAndPolynomials
                     coeff[i] = obj1.Coefficients[i] - obj2.Coefficients[i];
                 }
                 for (int i = obj1.Coefficients.Length; i < obj2.Coefficients.Length; i++) coeff[i] = -obj2.Coefficients[i];
-                return new Polynomial(coeff);
+                return new Polynomial(coeff.Trim());
             }
 
         }
         public static Polynomial operator -(Polynomial obj, double value) => obj + (-value);
-        public static Polynomial operator -(double value, Polynomial obj) => obj + (-value); 
+        public static Polynomial operator -(double value, Polynomial obj) => obj + (-value);
+
+        public static Polynomial operator *(Polynomial obj1, Polynomial obj2)
+        {
+            double[] coeff = new double[obj1.Coefficients.Length + obj2.Coefficients.Length];
+
+            for (int i = 0; i < obj1.Coefficients.Length; i++)
+            {
+                for (int j = 0; j < obj2.Coefficients.Length; j++)
+                {
+                    coeff[i + j] += obj1.Coefficients[i] * obj2.Coefficients[j];
+                }
+            }
+
+            return new Polynomial(coeff.Trim());
+        }
+
+        public static Polynomial operator *(Polynomial poly, double value)
+        {
+            double[]coeff=new double[poly.Coefficients.Length];
+            for (int i = 0; i < poly.Coefficients.Length; i++)
+            {
+                coeff[i] = poly.Coefficients[i] * value;
+            }
+
+            return new Polynomial(coeff.Trim());
+        }
+
+        public static Polynomial operator *(double value, Polynomial poly) => poly * value;
+    }
+
+    public static class DoubleExtensions
+    {
+        public static double[] Trim(this double[] array)
+        {
+            if(array.Length==0)return array;
+            int index=0;
+            for (int i = array.Length - 1; i > 0; i--)
+            {
+                if(array[i]!=0) break;
+                index++;
+            }
+            if (index != 0)
+            {
+                double []newArr=new double[array.Length-index];
+                Array.Copy(array,0,newArr,0,array.Length-index);
+                array = newArr;
+            }
+            return array;
+        }
     }
 }
