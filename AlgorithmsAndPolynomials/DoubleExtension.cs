911aaa1f59e44d446b4c94a3268eec6426b7e0e2
Alexander <alexyarosh98@gmail.com>
2017-07-11 17:51:22 +0300

    Double extension method is fixed

--- AlgorithmsAndPolynomials/DoubleExtension.cs
+++ AlgorithmsAndPolynomials/DoubleExtension.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,8 +10,6 @@ namespace AlgorithmsAndPolynomials
 {
     public static class DoubleExtension
     {
-        private const string MAXMANTICA = "1111111111111111111111111111111111111111111111111111";
-        
         /// <summary>
         /// Representation a double number in a binary view
         /// </summary>
@@ -18,67 +17,34 @@ namespace AlgorithmsAndPolynomials
         /// <returns>string representation of number</returns>
         public static string ToBinaryView(this double number)
         {
-            if (number != number || double.IsInfinity(number) || number == double.MaxValue || number == double.MinValue||number==double.Epsilon)
-            {
-                return CheckArgumentValue(number);
-            }
-
-            string mantica = FindMantica(Math.Abs(number), out string wholePart);
-            string exponent = Convert.ToString(1023 + wholePart.Length-1, 2);
-            while (exponent.Length < 11) exponent += (number != 0?"0":"1") ;
-
-            return String.Format("{0}{1}{2}", (number >= 0) ? 0 : 1, exponent, mantica);
+            Dil newNumber = new Dil(number);
+            return BinaryViewOfWholePart(newNumber.longNumber);
         }
 
-
-        private static string CheckArgumentValue(double number)
+        private static string BinaryViewOfWholePart(long number)
         {
-            if (number == double.Epsilon) return "0001";
-            if (number != number) return "011111111111";
-
-            return String.Format("{0}11111111110{1}", number > 0 ? 0 : 1, MAXMANTICA);
-        }
-
-        private static string FindMantica(double number, out string wholePart)
-        {
-            StringBuilder res = new StringBuilder();
-
-            wholePart = BinaryViewOfWholePart(Math.Truncate(number));
-            string fractional = BinaryViewOfFraction(number % 1);
-
-            if (!wholePart.StartsWith("0")) res.Append(wholePart.Substring(1));
-            res.Append(fractional);
-
-            while (res.Length < 52)
-            {
-                res.Append("0");
-            }
-
-            return res.ToString();
-        }
-        private static string BinaryViewOfWholePart(double number)
-        {
-            long wholePart = (long)number;
-            if (wholePart == 0) return "0";
-            string result = string.Empty;
+            StringBuilder result = new StringBuilder();
 
             for (int i = 0; i < sizeof(long) * 8; i++)
-                result = ((wholePart & ((long)1 << i)) != 0 ? '1' : '0') + result;
+                result.Append(((number & ((long) 1 << 63 - i)) != 0 ? '1' : '0'));
 
-            return result.TrimStart(new[] { '0' });
+            return result.ToString();
         }
-        private static string BinaryViewOfFraction(double fractionalPart)
-        {
-            StringBuilder answer = new StringBuilder();
 
-            while (fractionalPart > 0)
+
+        [StructLayout(LayoutKind.Explicit)]
+        private struct Dil
+        {
+            [FieldOffset(0)] public long longNumber;
+
+            [FieldOffset(0)] private double doubleNumber;
+
+            public Dil(double number)
             {
-                fractionalPart *= 2;
-                answer.Append((int)fractionalPart);
-                fractionalPart -= (int)fractionalPart;
+                longNumber = 0;
+                doubleNumber = number;
             }
 
-            return answer.ToString();
         }
     }
 }
